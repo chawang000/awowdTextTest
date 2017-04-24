f8038d90db07131d2cbcd3323a2d169a0271d8d1
Zhaozhou Guo <yidanguo1102@gmail.com>
2017-04-24 10:33:33 -0700

    add web menu with basic scroll function and menu content(with canvas dust effect). book a life section is not finished yet

--- _js/menu.js
+++ _js/menu.js
@@ -3,11 +3,14 @@
 
 
 
+// MOBILE VAR
 var headerCanShow = true,
 	mobileMenuActived = false,
 	mobileBookActived = false,
 	mobileTransitioning = false,
 	bodyLocked = false;
+
+
 
 var mobileHeader = $('#mobileHeader'),
 	mobileMenu = $('#mobileMenu'),
@@ -17,14 +20,14 @@ var mobileHeader = $('#mobileHeader'),
 	mobileMenuContent = $('#mobileMenuContent'),
 	mobileUpperLogo = $('#mobileUpperLogo'),
 	menuLogo = $('#menuLogo'),
-	burgerToggle = $('.menu_icon'),
+	mobileBurgerToggle = $('#mobileHeader .menu_icon'),
 	mobileUpperClose = $('#mobileUpperClose'),//close button
 	mobileCloseText = $('#mobileCloseText'),
 	mobileUpperWrapper = $('#mobileUpperWrapper'),
 	mobileBookLife = $('#mobileBookLife'),
 	mobileBookDisplay = $('#mobileBookDisplay'),
+	mobileBookLowerDisplay = $('#mobileBookLowerDisplay'),
 	mobileBookContent = $('#mobileBookContent'),
-	mobileBookText = $('#mobileBookText'),
 	mobileEmailOuter = $('#mobileEmailOuter'),
 	inputLeftWrapper= $('#inputLeftWrapper'),
 	menuBookTitle = $('#menuBookTitle'),
@@ -47,11 +50,30 @@ var mUpperHeight,
 
 
 
+// WEB VAR
+var webTransitioning = false,
+	webMenuActived = false,
+	webBookActived = false;
+
+var webHeaderDisplay = $('#webHeaderDisplay'),
+	webMenu = $('#webMenu'),
+	webMenuDisplay = $('#webMenuDisplay'),
+	webMenuContent = $('#webMenuContent'),
+	webMenuInnerContent = $('#webMenuInnerContent'),
+	webBookLifeDisplay = $('#webBookLifeDisplay'),
+	webBookButton = $('#webBookButton'),
+	webBurgerToggle = $('#webHeader .menu_icon');
+
+
+var webMenuHeight;
+
+
 $(window).on("load", function (e){
-	mobileMenuSetter();
-	mobileMenuToggle(burgerToggle);
-	mobileBookToggle(menuBookTitle,mobileUpperClose,burgerToggle);
-	mobileBookToggle(mobileEmailOuter,mobileUpperClose,burgerToggle);
+	// MOBILE
+	mobileHeaderSetter();
+	mobileMenuToggle(mobileBurgerToggle);
+	mobileBookToggle(menuBookTitle,mobileUpperClose,mobileBurgerToggle);
+	mobileBookToggle(mobileEmailOuter,mobileUpperClose,mobileBurgerToggle);
 
 	$(window).resize(function(){
 	});
@@ -60,14 +82,154 @@ $(window).on("load", function (e){
 	$(document).on('touchmove scroll', function(){
 		headerStatesController(mobileHeader, (mUpperHeight + mLowerHeight));
 	});
-
 	menuBookInputBox.find('input')[0].addEventListener("focus", bookFocusStyle);
 	menuBookInputBox.find('input')[0].addEventListener("blur", bookBlurStyle);
+
+
+	// WEB
+	webHeaderSetter();
+	drawCanvas();
+	webMenuToggle(webBurgerToggle);
+	// webBookToggle(webBookButton);
+
+	$(window).resize(function(){
+		webHeaderSetter();
+	});
+
 });
 
+// =========================================================================
+// =========================================================================
+// ===============================WEB MENU==================================
+// =========================================================================
+// =========================================================================
+function webHeaderSetter(){
+	webHeaderHeight = webHeaderDisplay.height();
+	webMenuHeight = $(window).height() - webHeaderHeight;
+	webMenuDisplay.css({
+		'height':webMenuHeight + 'px'
+	});
+	// console.log(webMenuHeight);
+	webMenuTextHeight = $('#webMenuInnerContent ul').height();
+	webMenuInnerContent.css({
+		'height':webMenuTextHeight+'px'
+	});
+	if(webMenuActived){
+		webMenuDisplay.css({
+			'transform':'translate(0px, ' + webMenuHeight + 'px)'
+		});
+		webMenuContent.css({
+			'transform':'translate(0px, ' + -webMenuHeight + 'px)'
+		});
+	}
+}
+
+function webBookToggle(webBookButton){
+	webBookButton.on({
+	    'click': function(){
+	    	var menuTransformDistance = webMenuHeight;
+	        if(webMenuActived && !webTransitioning){
+	        	// releaseScroll();
+	        	// webMenuClose(webBurgerToggle);
+	        }else if(!webBookActived && !webTransitioning){
+	        	webBookOpen(menuTransformDistance);
+	        }
+	    }
+	});
+}
+
+function webBookOpen(menuTransformDistance){
+	// console.log('showing');
+	webTransitioning = true;
+	var element = webBookLifeDisplay[0];
+	element.addEventListener("transitionend", transitionCallback, false);
+
+	webBookShow(menuTransformDistance);
+}
 
 
+function webBookShow(menuTransformDistance){
+	webMenuDisplay.css({
+		'transform':'translate(0px, ' + menuTransformDistance + 'px)'
+	});
+	webBookLifeDisplay.css({
+		'transform':'translate(0px, ' + -menuTransformDistance + 'px)'
+	});
+}
 
+
+function webMenuToggle(webBurgerToggle){
+	webBurgerToggle.on({
+	    'click': function(){
+	    	var menuTransformDistance = webMenuHeight;
+	        if(webMenuActived && !webTransitioning){
+	        	releaseScroll();
+	        	webMenuClose(webBurgerToggle)
+	        }else if(!webMenuActived && !webTransitioning){
+	            webMenuOpen(webBurgerToggle,menuTransformDistance);
+	        }
+	    }
+	});
+}
+
+
+function  webMenuOpen(webBurgerToggle,menuTransformDistance){
+	webTransitioning = true;
+	var element = webMenuContent[0];
+	element.addEventListener("transitionend", transitionCallback, false);
+
+	webBurgerToggle.addClass('active');
+	
+	webMenuShow(menuTransformDistance);
+	function transitionCallback(){
+		element.removeEventListener("transitionend", transitionCallback, false);
+		webMenuActived = true;
+		webTransitioning = false;
+		lockScroll();
+	}
+}
+
+function webMenuClose(webBurgerToggle){
+	webTransitioning = true;
+	var element = webMenuContent[0];
+	element.addEventListener("transitionend", transitionCallback, false);
+
+	webBurgerToggle.removeClass('active');
+	webMenuLeave();
+
+	function transitionCallback(){
+		element.removeEventListener("transitionend", transitionCallback, false);
+		webMenuActived = false;
+		webTransitioning = false;
+	}
+}
+
+
+function webMenuShow(menuTransformDistance){
+
+	webMenuDisplay.css({
+		'transform':'translate(0px, ' + menuTransformDistance + 'px)'
+	});
+	webMenuContent.css({
+		'transform':'translate(0px, ' + -menuTransformDistance + 'px)'
+	});
+}
+
+function webMenuLeave(){
+	webMenuDisplay.css({
+		'transform':'translate(0px, ' + 0 + 'px)'
+	});
+	webMenuContent.css({
+		'transform':'translate(0px, ' + 0 + 'px)'
+	});
+}
+
+
+// =========================================================================
+// =========================================================================
+// ===============================MOBILE MENU===============================
+// =========================================================================
+// =========================================================================
 function headerStatesController(header, headerHeight){
 	var currentWindowScroll = $(window).scrollTop();
 	if(currentWindowScroll > 300 && !mobileTransitioning && currentWindowScroll < (documentHeight-windowH-100)){
@@ -114,7 +276,7 @@ function bookBlurStyle(){
 // -------------------------------------------------------------------------
 // * prevent scrolling background when the menu or book page is opening
 // =========================================================================
-function mobileMenuSetter(){
+function mobileHeaderSetter(){
 	windowH = $(window).height();
 	var mOutterWidth = mobileEmailOuter.width();
 	var mEmailSendWidth = menuBookSendButton.width();
@@ -145,15 +307,15 @@ function resetMenuHeights(){
 // -------------------------------------------------------------------------
 // * checking the mobile menu states
 // =========================================================================
-function mobileMenuToggle(burgerToggle){
-    burgerToggle[0].addEventListener('click', function(){
+function mobileMenuToggle(mobileBurgerToggle){
+    mobileBurgerToggle[0].addEventListener('click', function(){
     	var menuTransformDistance = mMenuHeight;
         if(mobileMenuActived && !mobileTransitioning){
         	releaseScroll();
-            mobileMenuClose(burgerToggle);
+            mobileMenuClose(mobileBurgerToggle);
         }else if(!mobileMenuActived && !mobileTransitioning){
         	lockScroll();
-            mobileMenuOpen(burgerToggle,menuTransformDistance);
+            mobileMenuOpen(mobileBurgerToggle,menuTransformDistance);
         }
     });
 }
@@ -167,7 +329,7 @@ function mobileMenuToggle(burgerToggle){
 // * toggle is the opening button and toggle2 is the closing button
 // * email functions
 // =========================================================================
-function mobileBookToggle(toggle,toggle2,burgerToggle){
+function mobileBookToggle(toggle,toggle2,mobileBurgerToggle){
 	toggle.on({
 		'mousedown': function(event) {
 			// blur();
@@ -186,7 +348,7 @@ function mobileBookToggle(toggle,toggle2,burgerToggle){
 	});
 
 	// blur the input if user touch the content area, disable the virtual keyboard
-	mobileBookText.on({
+	mobileBookContent.on({
 		'mousedown': function() {
 	        blur();
 	    },
@@ -219,7 +381,7 @@ function mobileBookToggle(toggle,toggle2,burgerToggle){
 		if(mobileMenuActived && !mobileBookActived && !mobileTransitioning){
 			// event.preventDefault();
 			releaseScroll();
-			mobileMenuClose(burgerToggle);
+			mobileMenuClose(mobileBurgerToggle);
 			lockScroll();
 			mobileBookOpen(closeTransformDistance,bookTransformDistance,toggle);
 		}else if(!mobileBookActived && !mobileTransitioning){
@@ -330,13 +492,13 @@ function mobileBookClose(){
 // * 2. after animation, animate menu content and lowerHeader
 // * 3. after transition, call transitionCallback to set states and remove listeners
 // =========================================================================
-function mobileMenuOpen(burgerToggle,menuTransformDistance){
+function mobileMenuOpen(mobileBurgerToggle,menuTransformDistance){
 	mobileTransitioning = true;
 	var element = document.getElementById("mobileLowerHeader");
 	element.addEventListener("transitionend", transitionCallback, false);
 
 	
-	burgerToggle.addClass('active');
+	mobileBurgerToggle.addClass('active');
 	mobileUpperHeader.animate({
 		height: mUpperHeight + mHeaderOffset +'px'
 	},200,function(){
@@ -353,14 +515,14 @@ function mobileMenuOpen(burgerToggle,menuTransformDistance){
 
 
 
-function mobileMenuClose(burgerToggle){
+function mobileMenuClose(mobileBurgerToggle){
 	var element = document.getElementById("mobileLowerHeader");
 	element.addEventListener("transitionend", transitionCallback, false);
 	mobileTransitioning = true;
 
 	
 	mobileMenuLeave();
-	burgerToggle.removeClass('active');
+	mobileBurgerToggle.removeClass('active');
 	function transitionCallback(){
 		// mobileUpperHeader.removeClass('shadow');
 		// mobileLowerHeader.removeClass('shadow');
@@ -481,7 +643,7 @@ function mobileBookShow(bookTransformDistance){
 	mobileBookDisplay.css({
 		'transform':'translate(0px, ' + bookTransformDistance + 'px)',
 	});
-	mobileBookContent.css({
+	mobileBookLowerDisplay.css({
 		'transform':'translate(0px, ' + -bookTransformDistance + 'px)',
 	});
 }
@@ -491,7 +653,7 @@ function mobileBookLeave(){
 	mobileBookDisplay.css({
 		'transform':'translate(0px, ' + 0 + 'px)',
 	});
-	mobileBookContent.css({
+	mobileBookLowerDisplay.css({
 		'transform':'translate(0px, ' + 0 + 'px)',
 	});
 }
@@ -519,3 +681,192 @@ function bookTitleIn(){
 }
 
 
+
+
+
+// CANVAS PARTICLES
+
+
+//Dust Particles Simulation by bionicoz based on
+//Basic Particle Animation
+//Author: Brandon John-Freso
+function drawCanvas(){
+    var W, H,
+        canvas, ctx, //ctx stands for context and is the "curso" of our canvas element.
+        particleCount = 800,
+        particles = []; //this is an array which will hold our particles Object/Class
+
+    W = window.innerWidth ;
+    H = webMenuHeight ;
+    
+    canvas = $("#canvas").get(0); //this "get(0) will pull the underlying non-jquery wrapped dom element from our selection
+    canvas.width = W;
+    canvas.height = H;
+
+    ctx = canvas.getContext("2d"); // settng the context to 2d rather than the 3d WEBGL
+    ctx.globalCompositeOperation = "lighter";
+    var mouse = {
+      x: 0, 
+      y: 0,
+      rx:0,
+      ry:0,
+      speed:45,
+      delta:0
+    };
+    
+
+
+    document.addEventListener('mousemove', function(e){ 
+        
+        mouse.x = e.clientX || e.pageX; 
+        mouse.y = e.clientY || e.pageY;
+        mouse.x-=W/2;
+        mouse.y-=H/2;
+      
+    }, false);
+  
+    function randomNorm(mean, stdev) {
+      
+      return Math.abs(Math.round((Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1))*stdev)+mean;
+    }
+
+    //Setup particle class
+    function Particle() {
+        //using hsl is easier when we need particles with similar colors
+      	var randomNum = Math.random();
+        this.r=parseInt(100 * randomNum + 0);
+        this.g=parseInt(100 * randomNum + 0);
+        this.b=parseInt(100 * randomNum + 0);
+        this.a=0.7*Math.random()+0.3;
+      
+        this.color = "rgba("+ this.r +","+ this.g +","+ this.b +","+(this.a)+")";
+        this.shadowcolor = "rgba("+ this.r +","+ this.g +","+ this.b +","+parseFloat(this.a-0.6)+")";
+      
+
+        
+        this.x = Math.random() * W;
+        this.y = Math.random() * H;
+        this.direction = {
+            "x": -1 + Math.random() * 2,
+            "y": -1 + Math.random() * 2
+        };
+        //this.radius = 9 * ((Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1)+3);
+        this.radius = randomNorm(0,0.8);
+        this.scale=0.2*Math.random()+0.8;
+        this.rotation=Math.PI/4*Math.random();
+      
+        this.grad=ctx.createRadialGradient( this.x, this.y, this.radius, this.x, this.y, 0 );
+        this.grad.addColorStop(0,this.color);
+        this.grad.addColorStop(1,this.shadowcolor);
+      
+        this.vx = (10 * Math.random() + 10)*.01*this.radius;
+        this.vy = (10 * Math.random() + 10)*.01*this.radius;
+        
+        this.valpha = 0.01*Math.random()-0.02;
+        
+        this.move = function () {
+            this.x += this.vx * this.direction.x ;
+            this.y += this.vy * this.direction.y ;
+            this.rotation+=this.valpha;
+            //this.radius*= Math.abs((this.valpha*0.01+1));
+
+        };
+        this.changeDirection = function (axis) {
+            this.direction[axis] *= -1;
+            this.valpha *= -1;
+        };
+        this.draw = function () {
+            ctx.save();
+            ctx.translate(this.x+mouse.rx/-20*this.radius,this.y+mouse.ry/-20*this.radius);  
+          ctx.rotate(this.rotation);  
+          ctx.scale(1,this.scale);
+            
+            this.grad=ctx.createRadialGradient( 0, 0, this.radius, 0, 0, 0 );
+            this.grad.addColorStop(1,this.color);
+            this.grad.addColorStop(0,this.shadowcolor);
+            ctx.beginPath();
+            ctx.fillStyle = this.grad;
+            ctx.arc(0, 0, this.radius, 0, Math.PI * 2, false);
+            ctx.fill();
+            ctx.restore();
+        };
+        this.boundaryCheck = function () {
+            if (this.x >= W*1.2) {
+                this.x = W*1.2;
+                this.changeDirection("x");
+            } else if (this.x <= -W*0.2) {
+                this.x = -W*0.2;
+                this.changeDirection("x");
+            }
+            if (this.y >= H*1.2) {
+                this.y = H*1.2;
+                this.changeDirection("y");
+            } else if (this.y <= -H*0.2) {
+                this.y = -H*0.2;
+                this.changeDirection("y");
+            }
+        };
+    } //end particle class
+
+    function clearCanvas() {
+        ctx.clearRect(0, 0, W, H);
+    } //end clear canvas
+
+    function createParticles() {
+        for (var i = particleCount - 1; i >= 0; i--) {
+            p = new Particle();
+            particles.push(p);
+        }
+    } // end createParticles
+
+    function drawParticles() {
+        for (var i = particleCount - 1; i >= 0; i--) {
+            p = particles[i];
+            p.draw();
+        }      
+    } //end drawParticles
+
+    function updateParticles() {
+        for (var i = particles.length - 1; i >= 0; i--) {
+            p = particles[i];
+            p.move();
+            p.boundaryCheck();
+
+        }
+    } //end updateParticles
+
+    function initParticleSystem() {
+        createParticles();
+        drawParticles();
+    }
+
+    function animateParticles() {
+        clearCanvas();
+        setDelta();
+        update()
+        drawParticles();
+        updateParticles();
+        requestAnimationFrame(animateParticles);
+    }
+  
+    initParticleSystem();
+    requestAnimationFrame(animateParticles);
+  
+    function setDelta() {  
+      this.now    =   (new Date()).getTime();  
+      mouse.delta  =   (this.now-this.then)/1000;  
+      this.then   =   this.now;  
+    }
+    function update() {  
+ 
+	    if(isNaN(mouse.delta) || mouse.delta <= 0) { return; }  
+	 
+	    var distX   =   mouse.x - (mouse.rx),  
+	        distY   =   mouse.y - (mouse.ry);  
+
+	    if(distX !== 0 && distY !== 0) {          
+	        mouse.rx -=  ((mouse.rx - mouse.x) / mouse.speed); 
+	        mouse.ry -=  ((mouse.ry - mouse.y) / mouse.speed); 
+	    }   
+	};  
+};
