a6038adfbc6f48feef61d844337bf74d92b41eab
Zhaozhou Guo <yidanguo1102@gmail.com>
2017-05-17 18:52:00 -0700

    same with last commit

--- _js/deer.js
+++ _js/deer.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	// console.log('ready');
-	// <animateMotion path="M 0 0 H -5 Z" dur="3s" repeatCount="indefinite"></animateMotion>
-	var allCircles = $('svg #dots circle');
-	var allPolygons = $('svg #lines polygon');
 	var allShapes = $('svg #shapes polygon');
 	var shapesInfo = [];
 	var allPolyline = $('svg #shapes polyline');
@@ -21,7 +17,6 @@ $(document).ready(function(){
 
 	$('#homeSection_2').on('mouseenter',function(){
 		for(var i = 0; i < allShapes.length; i++){
-			var randomTime = 1 + Math.random()*2;
 			$(allShapes[i]).css({
 				'transform':'translate(' +shapesInfo[i].targetX+'px, '+shapesInfo[i].targetY+'px)',
 				// 'transition':'all ' + randomTime + 's'
@@ -38,8 +33,6 @@ $(document).ready(function(){
 			var colorR = 100+parseInt(Math.random()*155);
 			var colorG = 200+parseInt(Math.random()*15);
 			var colorB = 0+parseInt(Math.random()*10);
-			// var colorA = $(target[i]).css('opacity');
-			// console.log(target1);
 
 			if(target1 != null){
 				changeIndividualColor(target1);
@@ -56,11 +49,8 @@ $(document).ready(function(){
 					$(target[i]).css({
 						fill:'rgba('+colorR+','+colorG+','+colorB+','+orgOpacity+')',
 						opacity:targetOpacity,
-						transition: "all "+randomSpeed/800+'s'
+						transition: "all "+randomSpeed/2000+'s'
 					});
-					// $(target[i]).stop(true,true).animate({
-					// 	opacity:targetOpacity,
-					// },randomSpeed);
 				}
 			}
 		}
@@ -68,17 +58,16 @@ $(document).ready(function(){
 
 
 	$('#homeSection_2').on('mouseleave',function(){
-		var randomTime = 1 + Math.random()*1;
+		
 		for(var i = 0; i < allShapes.length; i++){
+			var randomTime = 0 + Math.random()*4;
 			$(allShapes[i]).css({
 				'transform':'translate(' +0+'px, '+0+'px)',
 				'transition':'all ' + randomTime + 's'
 			});
 		}
 		$('svg #lines').css({
-			opacity:0.3,
-			transition:'opacity 1s',
-			webkitTransition: 'opacity 1s', 
+			opacity:0.3
 		});
 
 		changefillBW(0.3,0.1,allShapes,allPolyline);
@@ -96,20 +85,13 @@ $(document).ready(function(){
 			function changeIndividualColor(target){
 				for(var i = 0; i < target.length; i++){
 					var classOpacity = shapesInfo[i].shapeOpacity;
-					// console.log();
-					var randomSpeed = Math.random()*2000;
 					$(target[i]).css({
 						fill:'#000',
-						opacity:classOpacity,
-						// transition: 'all '+randomSpeed/1000+'s'
+						opacity:classOpacity
 					});
-					// $(target[i]).stop(true,true).animate({
-					// 	opacity:classOpacity,
-					// },randomSpeed/2);
 				}
 			}
 		}
-
 	});
 });
 
